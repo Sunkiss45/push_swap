076b8475271e9ee94ac2913beda0616dbb22a2ba
Elouann BARGUILLET <ebarguil@e3r10p12.42.fr>
2021-11-16 17:41:04 +0100

    161121-1741

--- algo/mort.c
+++ algo/mort.c
@@ -6,12 +6,12 @@
 /*   By: ebarguil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 16:18:43 by ebarguil          #+#    #+#             */
-/*   Updated: 2021/11/16 14:28:34 by ebarguil         ###   ########.fr       */
+/*   Updated: 2021/11/16 17:39:39 by ebarguil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../push_swap.h"
 
-static int	ft_calc_rot(t_adm *adm, int i)
+int	ft_calc_rot(t_adm *adm, int i)
 {
 	t_dll	*now;
 	int		c;
@@ -32,6 +32,32 @@ static int	ft_calc_rot(t_adm *adm, int i)
 	return (0);
 }
 
+t_dll	*pos_b(t_dll *pa, t_adm *admb)
+{
+	t_dll	*pb;
+	int		m[2];
+
+	pb = admb->head;
+	m[0] = INT_MAX;
+	m[1] = 0;
+	while (pb != admb->tail)
+	{
+		if (pb->i < m[0])
+			m[0] = pb->i;
+		if (pb->i > m[1])
+			m[1] = pb->i;
+		if (pa->i > pb->i && pa->i < pb->prev->i)
+			return (pb);
+		pb = pb->next;
+	}
+/* FAIRE GAFFE A TAIL */
+	pb = admb->head;
+	if (pa->i < m[0] || pa->i > m[1])
+		while (pb->i != m[1])
+			pb = pb->next;
+	return (pb);
+}
+
 void	mort(t_adm *adma, t_adm *admb)
 {
 	t_dll	*now[2];
@@ -40,11 +66,7 @@ void	mort(t_adm *adma, t_adm *admb)
 	now[0] = adma->head;
 	while(now[0] != adma->tail)
 	{
-		now[1] = admb->head;
-		while(now[1] != admb->tail && now[0]->i < now[1]->i)
-			now[1] = now[1]->next;
-		if (now[1] == admb->tail)
-			now[1] = admb->head;
+		now[1] = pos_b(now[0], admb);
 		ft_calc_rot(adma, 0);
 		now[0] = now[0]->next;
 	}
